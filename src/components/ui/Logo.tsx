efc1d3cd9384b6ef25193908ba15c1cb4b935264
Jordan Kowal <kowaljordan@gmail.com>
2025-09-22 12:11:43 +0200

    fix: alt text for img

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,5 +2,5 @@ import type React from "react";
 import { memo } from "react";
 
 export const Logo: React.FC = memo(() => {
-  return <img data-testid="logo" src="/logo.png" alt="JKDev Logo" />;
+  return <img data-testid="logo" src="/logo.png" alt="Click Launch Logo" />;
 });
